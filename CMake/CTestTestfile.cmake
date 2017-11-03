dd173ab579cbb8fc4a83d93a44df973a0a020bb9
andrejj7771 <andrejj6661@gmail.com>
2017-11-03 16:05:13 +0700

    delete CMake dir

--- CMake/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrey/TestTask
-# Build directory: /home/andrey/TestTask/CMake
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(test)
